84b82c6efd0c2cf4ad63ab36638860e9245c5451
amaurygarcia <amaurygp9@gmail.com>
2024-01-20 22:07:27 -0500

    subida de proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyC3sW_DiYZ7zjN8WzcKQXuSOMf1D5FsMU4",
+    authDomain: "product-admin-app-1aa32.firebaseapp.com",
+    projectId: "product-admin-app-1aa32",
+    storageBucket: "product-admin-app-1aa32.appspot.com",
+    messagingSenderId: "744977461309",
+    appId: "1:744977461309:web:6f1dc2b4b28df87f6f7a54"
+  }
 };
 
 /*
